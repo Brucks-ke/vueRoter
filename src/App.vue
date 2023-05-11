dee71a09d7ad2d5415964bba0046cd51513bc24d
Brucks-ke <122675744+Brucks-ke@users.noreply.github.com>
2023-05-11 15:34:30 +0800

    [vue3初始化]

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <h1>首页</h1>
+  <div>{{aa}}</div>
   <router-view></router-view>
 </template>
 
 <style></style>
 
-<script lang="ts" setup>
+<script>
+import {ref} from "vue"
+export default{
+  setup(){
+    const aa = ref("小明")
+    return {
+      aa
+    }
+  }
+}
+
+
+
 
 </script>
